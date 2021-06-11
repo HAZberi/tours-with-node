357aa648efb5d7a7ce4d3ad5d8aad228e0cdb0fc
Hassaan Zuberi <mhazuberi@gmail.com>
2021-06-10 22:18:35 -0600

    get requests and creating data/api endpoint

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log(req);
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
-    message: "Hello from the server side...",
-    appName: "tours with node",
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
 });
 
-app.post("/", (req, res) => {
-  res.send("You can post the data to this endpoint.");
-});
+// app.get("/", (req, res) => {
+//   console.log(req);
+//   res.status(200).json({
+//     message: "Hello from the server side...",
+//     appName: "tours with node",
+//   });
+// });
+
+// app.post("/", (req, res) => {
+//   res.send("You can post the data to this endpoint.");
+// });
 
 const port = 4000;
 
