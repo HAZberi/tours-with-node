2ca024f4358148e6808d0307845fcb2944753f75
Hassaan Zuberi <mhazuberi@gmail.com>
2021-11-14 21:10:29 +0300

    major cleanup for swapping db connection with fs

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -7,11 +7,10 @@ const {
   updateATour,
   deleteATour,
   createATour,
-  checkId,
   checkBody,
 } = require('../controllers/tourController');
 
-router.param('id', checkId);
+//router.param('id', checkId);
 
 router.route('/').get(getAllTours).post(checkBody, createATour);
 
