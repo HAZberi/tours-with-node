4c65d2c10e26f330d82889a84b3606abb0ffdc0a
Hassaan Zuberi <mhazuberi@gmail.com>
2021-11-15 18:52:20 +0300

    data import and delete from json implemented

--- dev-data/data/manage-dev-data.js
+++ dev-data/data/manage-dev-data.js
@@ -5,10 +5,36 @@ dotenv.config();
 const fs = require('fs');
 //const mongoose = require('mongoose');
 const connectDB = require('../../db');
+const Tour = require('../../models/tourModel');
 //const Tour = require('../../models/tourModel');
 
 connectDB();
 
-const tours = fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
 
-console.log(tours);
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully imported.');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('All documents in tour collection successfully deleted.');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+}
+if (process.argv[2] === '--delete') {
+  deleteData();
+}
