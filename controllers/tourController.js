e47f080386c523abd80ca324a50f60e72b7dbd78
Hassaan Zuberi <mhazuberi@gmail.com>
2021-11-16 18:18:23 +0300

    specific fields as response

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,6 +22,16 @@ exports.getAllTours = async (req, res) => {
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //6. Select specific fields for the response
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
 
     //Execute Query
