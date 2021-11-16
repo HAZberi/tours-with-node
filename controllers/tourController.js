2cddcd92d449b679b33ff6e4ea61ac5e774d29c2
Hassaan Zuberi <mhazuberi@gmail.com>
2021-11-16 17:26:11 +0300

    implemented advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,16 +2,21 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     //Build Query
     //1. Built a DEEP COPY of query request object
     const queryObj = { ...req.query };
 
-    //2. Exclude special words for data fields
+    //2. Filtering Exclude special words for data fields
     const excludeSpecialWords = ['sort', 'limit', 'page', 'fields'];
     excludeSpecialWords.forEach((el) => delete queryObj[el]);
 
-    //3. Create Query
-    const query = Tour.find(queryObj);
+    //3. Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|lte|lt|gt)\b/g, (match) => `$${match}`);
+
+    //4. Create Query
+    const query = Tour.find(JSON.parse(queryStr));
 
     //Execute Query
     const tours = await query;
