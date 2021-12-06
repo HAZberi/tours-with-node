cdef6f2fa2ddaacddbfcb3f6461f44e657e96f91
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-06 18:01:13 +0300

    added comment to clarify error handling workflow

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,4 +1,5 @@
 //an example of function currying.
+//This function catches error and then the error is handled through globalErrorhandler middleware.
 module.exports = (fn) => (req, res, next) => {
   fn(req, res, next).catch((err) => next(err));
 };
