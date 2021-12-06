164db998ce0577f910b046583e6393cf2a281839
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-06 19:10:27 +0300

    separate error handlers for prod and dev env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,39 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //if the error is operational => Trusted Error, send message to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //if the error is a programming error then send a generic error to the client
+    //1. Log Error to the console
+    console.error('ERROR:', err);
+
+    //2. Send a very generic message to the client
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went very wrong!!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
