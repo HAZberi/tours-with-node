363e09c88bf6db92761a98bfb04722505bf64a15
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-08 18:09:56 +0300

    implemented uncaughtException handler 4 non-async

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+process.on('uncaughtException', (err) => {
+  //This callback is to handle any synchronous errors that are not handled on the server.
+  //It importatnt to run this function at the start of the application since its meant to handle synchronous stuff.
+  console.log('Uncaught Exception Occurred. Shutting down the server.');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 // This file contains all the configuration about running the nodeJS
 // server and import appropriate environmental variables
 
@@ -15,6 +23,14 @@ const app = require('./app');
 
 //console.log(process.env);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Listening requests on port ${process.env.PORT}.... `);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection Promise. Shutting down the server.');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
