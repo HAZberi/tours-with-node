d92e21abd0052e18ebf3226ecfe3eedd211e9a28
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-14 23:04:14 +0300

    Prevent req.body pollution Protected user creation with req.body destructuring

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,22 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  //The following code creates a user based on req.body but any user can also create admin user by this technique.
+  //https://www.mongodb.com/features/mongodb-authentication
+  //https://www.guru99.com/mongodb-create-user.html
+  //Anyone can manipulate the req.body to assign admin roles which is a security issue.
+  //const newUser = await User.create(req.body);
+
+  //Avoid a potential breach by creating user in a more specific way as follows, so no one can assign roles in req.body
+  const { name, email, photo, password, confirmPassword } = req.body;
+
+  const newUser = await User.create({
+    name,
+    email,
+    photo,
+    password,
+    confirmPassword,
+  });
 
   res.status(201).json({
     status: 'success',
