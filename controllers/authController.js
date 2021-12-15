46aef99a3cde6271fcd49e57796278efe3539158
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-15 20:22:25 +0300

    Created jwt token for client in sign up process. Created EXPIRES_IN option in .env

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -19,8 +20,14 @@ exports.signUp = catchAsync(async (req, res, next) => {
     confirmPassword,
   });
 
+  //Generate a jwt token for the client as follows
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
