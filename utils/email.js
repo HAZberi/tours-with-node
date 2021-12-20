95d7f34d1697c224ccc0dafe4bcba44ed625b9dd
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-20 10:35:33 +0300

    Sending Reset Token Email testing smtp server options integrated

--- utils/email.js
+++ utils/email.js
@@ -3,12 +3,14 @@ const nodemailer = require('nodemailer');
 const sendEmail = (options) => {
   //1) Create a transporter
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
   });
+  //Activate service: 'GMAIL' in less secure app
   //2) Define the email options
   //3) Actually send the email
 };
