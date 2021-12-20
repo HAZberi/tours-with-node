52a29def822420a25c90d73e6811783700f16723
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-20 15:17:45 +0300

    corrected json response for updateMe controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,7 +31,9 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: 'success',
-    user: updatedUser,
+    data: {
+      user: updatedUser,
+    },
   });
 });
 
