79bc5adf30258a1b531a750f6116b9b7657216b2
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-20 10:52:34 +0300

    Send Token via Email nodemailer email sending module setup complete

--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = (options) => {
+const sendEmail = async (options) => {
   //1) Create a transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
@@ -12,5 +12,17 @@ const sendEmail = (options) => {
   });
   //Activate service: 'GMAIL' in less secure app
   //2) Define the email options
+
+  const mailOptions = {
+    from: 'Hassaan Zuberi <mhazuberi@yahoo.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    //html: html body can be send too
+  };
+
   //3) Actually send the email
+  await transporter.sendMail(mailOptions);
 };
+
+module.exports = sendEmail;
