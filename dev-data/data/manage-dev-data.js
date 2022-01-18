bf54f52195583956e45d5b0d7420bff23dffd0dd
Hassaan Zuberi <mhazuberi@gmail.com>
2022-01-18 20:10:34 +0300

    imported user and review data passed all the tests

--- dev-data/data/manage-dev-data.js
+++ dev-data/data/manage-dev-data.js
@@ -6,15 +6,23 @@ const fs = require('fs');
 //const mongoose = require('mongoose');
 const connectDB = require('../../db');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 //const Tour = require('../../models/tourModel');
 
 connectDB();
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully imported.');
   } catch (err) {
     console.error(err);
@@ -24,6 +32,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('All documents in tour collection successfully deleted.');
   } catch (err) {
     console.error(err);
