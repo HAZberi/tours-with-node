ed5b7a4f0977871372c82b24773a03581f6182cc
Hassaan Zuberi <mhazuberi@gmail.com>
2022-01-27 06:51:25 +0300

    Calculate quantities and averages for ratings. Implemented a way to calculate stats in one-many relationships. Logging the results in console and testing.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -48,6 +48,40 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+//Assign a static function on the schema to calculate/aggregate/ something from the collection.
+//DONOT USE ARROW FUNCTION here, it messes up with the value of 'this'
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  //'this' refers to the Model here
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  console.log(stats);
+};
+
+//Triggers once the doucment is saved to the database.
+reviewSchema.post('save', (doc, next) => {
+  //Since calcAverageRatings is defined on the model and not on the document.
+  //In order to call calcAverageRatings, access to the model object is required.
+  //Direct access to the model is not available but statics will be available on the constructor that created this document.
+
+  //Data and methods required to calculate average ratings on the tour.
+  // console.log(doc.constructor);
+  // console.log(doc.tour);
+
+  doc.constructor.calcAverageRatings(doc.tour);
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
